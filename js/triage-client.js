6bf442baf5d8bd5be0ff11e559d23cc3a71e8575
jamison.lu <jamisonlu@hotmail.com>
2011-12-16 10:38:25 +1100

    Migrated js client into submodule

---
+++ js/triage-client.js
@@ -0,0 +1,183 @@
+var Triage = function () {
+	"use strict";
+
+	var Triage = {
+		application: '',
+		host: '',
+		language: 'JAVASCRIPT',
+		portal: null,
+		queue: [],
+
+		init: function (host, application) {
+			this.host = host;
+			this.application = application;
+
+			var self = this;
+
+			this.portal = new Triage.Portal(this.host);
+
+			window.onerror = function (msg, url, line) {
+				self.logError(msg, url, line);
+			};
+		},
+
+		logError: function (msg, url, line, severity) {
+			this.getPortal().request( this._urlData('error', msg, url, line, severity) );
+		},
+
+		logMsg: function (msg, url, line) {
+			this.getPortal().request( this._urlData('msg', msg, url, line) );
+		},
+
+		_urlData: function (name, exception, url, line, severity) {
+			var data = new Triage.UrlData();
+			data
+				.add('application', this.application)
+				.add('host', this.host)
+				.add('language', this.language)
+				.add('level', name)
+				.add('line', line)
+				.add('type', 'error')
+				.add('message', exception)
+				.add('context', {
+					'url' : url,
+					'useragent' : navigator.userAgent,
+					'cookies' : document.cookie,
+					'host' : this.host
+				})
+
+			//Print stack trace if external printStackTrace library is available
+			if(printStackTrace){
+				data.add('stacktrace', printStackTrace());
+			}
+
+			if (severity) {
+				data.add('severity', severity)
+			}
+
+			return '?data='+data.output();
+		},
+
+		getPortal: function(){
+			return this.portal;
+		}
+	}
+
+	Triage.UrlData = function() {
+		var data = {};
+
+		this.add = function (name, value) {
+			data[name] = value;
+
+			return this;
+		}
+
+		this.output = function(){
+			console.log(msgpack.pack({}))
+			return msgpack.pack(data);
+		}
+
+		return this;
+	}
+
+	Triage.Portal = function(host){
+		var queue = [];
+		var iframe = null;
+		var self = this;
+
+		/** Setup iframe **/
+		iframe = document.createElement('iframe');
+		iframe.style.width   = '1px';
+		iframe.style.height  = '1px';
+		iframe.style.display = 'none';
+
+		iframe.onload = function() {
+			if(queue.length > 0){
+				self.executeRequest();
+			}
+		}
+
+		if (document.body) {
+			document.body.appendChild(iframe);
+		} else {
+			_onWindowLoad(function(){
+				document.body.appendChild(iframe);
+			});
+		}
+
+		/** public functions **/
+
+		this.request = function(data){
+			queue.push(_getUrl(data));
+
+			if (queue.length === 1) {
+				this.executeRequest();
+			}
+		}
+
+		this.executeRequest = function() {
+			iframe.src = queue[0];
+			queue.shift();
+		}
+
+		/** private functions **/
+
+		function _onWindowLoad(callback) {
+			var previousOnload = window.onload;
+
+			if(typeof window.onload != 'function'){
+				window.onload = callback;
+				return;
+			}
+
+			window.onload = function(){
+				if(previousOnload){
+					previousOnload();
+				}
+
+				callback();
+			};
+		}
+
+		function _getUrl(data){
+			return 'http://'+host + '/api/log'+data;
+		}
+
+		return this;
+	}
+
+	// Domain Public by Eric Wendelin http://eriwen.com/ (2008)
+	//                  Luke Smith http://lucassmith.name/ (2008)
+	//                  Loic Dachary <loic@dachary.org> (2008)
+	//                  Johan Euphrosine <proppy@aminche.com> (2008)
+	//                  Oyvind Sean Kinsey http://kinsey.no/blog (2010)
+	//                  Victor Homyakov <victor-homyakov@users.sourceforge.net> (2010)
+
+	/**
+	 * Main function giving a function stack trace with a forced or passed in Error
+	 *
+	 * @cfg {Error} e The error to create a stacktrace from (optional)
+	 * @cfg {Boolean} guess If we should try to resolve the names of anonymous functions
+	 * @return {Array} of Strings with functions, lines, files, and arguments where possible
+	 */
+	function printStackTrace(a){var a=a||{guess:!0},b=a.e||null,a=!!a.guess,d=new printStackTrace.implementation,b=d.run(b);return a?d.guessAnonymousFunctions(b):b}printStackTrace.implementation=function(){};
+	printStackTrace.implementation.prototype={run:function(a,b){a=a||this.createException();b=b||this.mode(a);return"other"===b?this.other(arguments.callee):this[b](a)},createException:function(){try{this.undef()}catch(a){return a}},mode:function(a){return a.arguments&&a.stack?"chrome":"string"===typeof a.message&&"undefined"!==typeof window&&window.opera?!a.stacktrace?"opera9":-1<a.message.indexOf("\n")&&a.message.split("\n").length>a.stacktrace.split("\n").length?"opera9":!a.stack?"opera10a":0>a.stacktrace.indexOf("called from line")?
+	"opera10b":"opera11":a.stack?"firefox":"other"},instrumentFunction:function(a,b,d){var a=a||window,c=a[b];a[b]=function(){d.call(this,printStackTrace().slice(4));return a[b]._instrumented.apply(this,arguments)};a[b]._instrumented=c},deinstrumentFunction:function(a,b){if(a[b].constructor===Function&&a[b]._instrumented&&a[b]._instrumented.constructor===Function)a[b]=a[b]._instrumented},chrome:function(a){a=(a.stack+"\n").replace(/^\S[^\(]+?[\n$]/gm,"").replace(/^\s+at\s+/gm,"").replace(/^([^\(]+?)([\n$])/gm,
+	"{anonymous}()@$1$2").replace(/^Object.<anonymous>\s*\(([^\)]+)\)/gm,"{anonymous}()@$1").split("\n");a.pop();return a},firefox:function(a){return a.stack.replace(/(?:\n@:0)?\s+$/m,"").replace(/^\(/gm,"{anonymous}(").split("\n")},opera11:function(a){for(var b=/^.*line (\d+), column (\d+)(?: in (.+))? in (\S+):$/,a=a.stacktrace.split("\n"),d=[],c=0,f=a.length;c<f;c+=2){var e=b.exec(a[c]);if(e){var g=e[4]+":"+e[1]+":"+e[2],e=e[3]||"global code",e=e.replace(/<anonymous function: (\S+)>/,"$1").replace(/<anonymous function>/,
+	"{anonymous}");d.push(e+"@"+g+" -- "+a[c+1].replace(/^\s+/,""))}}return d},opera10b:function(a){for(var b=/^(.*)@(.+):(\d+)$/,a=a.stacktrace.split("\n"),d=[],c=0,f=a.length;c<f;c++){var e=b.exec(a[c]);e&&d.push((e[1]?e[1]+"()":"global code")+"@"+e[2]+":"+e[3])}return d},opera10a:function(a){for(var b=/Line (\d+).*script (?:in )?(\S+)(?:: In function (\S+))?$/i,a=a.stacktrace.split("\n"),d=[],c=0,f=a.length;c<f;c+=2){var e=b.exec(a[c]);e&&d.push((e[3]||"{anonymous}")+"()@"+e[2]+":"+e[1]+" -- "+a[c+
+	1].replace(/^\s+/,""))}return d},opera9:function(a){for(var b=/Line (\d+).*script (?:in )?(\S+)/i,a=a.message.split("\n"),d=[],c=2,f=a.length;c<f;c+=2){var e=b.exec(a[c]);e&&d.push("{anonymous}()@"+e[2]+":"+e[1]+" -- "+a[c+1].replace(/^\s+/,""))}return d},other:function(a){for(var b=/function\s*([\w\-$]+)?\s*\(/i,d=[],c,f;a&&10>d.length;)c=b.test(a.toString())?RegExp.$1||"{anonymous}":"{anonymous}",f=Array.prototype.slice.call(a.arguments||[]),d[d.length]=c+"("+this.stringifyArguments(f)+")",a=a.caller;
+	return d},stringifyArguments:function(a){for(var b=[],d=Array.prototype.slice,c=0;c<a.length;++c){var f=a[c];void 0===f?b[c]="undefined":null===f?b[c]="null":f.constructor&&(f.constructor===Array?b[c]=3>f.length?"["+this.stringifyArguments(f)+"]":"["+this.stringifyArguments(d.call(f,0,1))+"..."+this.stringifyArguments(d.call(f,-1))+"]":f.constructor===Object?b[c]="#object":f.constructor===Function?b[c]="#function":f.constructor===String?b[c]='"'+f+'"':f.constructor===Number&&(b[c]=f))}return b.join(",")},
+	sourceCache:{},ajax:function(a){var b=this.createXMLHTTPObject();if(b)try{return b.open("GET",a,!1),b.send(null),b.responseText}catch(d){}return""},createXMLHTTPObject:function(){for(var a,b=[function(){return new XMLHttpRequest},function(){return new ActiveXObject("Msxml2.XMLHTTP")},function(){return new ActiveXObject("Msxml3.XMLHTTP")},function(){return new ActiveXObject("Microsoft.XMLHTTP")}],d=0;d<b.length;d++)try{return a=b[d](),this.createXMLHTTPObject=b[d],a}catch(c){}},isSameDomain:function(a){return-1!==
+	a.indexOf(location.hostname)},getSource:function(a){a in this.sourceCache||(this.sourceCache[a]=this.ajax(a).split("\n"));return this.sourceCache[a]},guessAnonymousFunctions:function(a){for(var b=0;b<a.length;++b){var d=/^(.*?)(?::(\d+))(?::(\d+))?(?: -- .+)?$/,c=a[b],f=/\{anonymous\}\(.*\)@(.*)/.exec(c);if(f){var e=d.exec(f[1]),d=e[1],f=e[2],e=e[3]||0;d&&this.isSameDomain(d)&&f&&(d=this.guessAnonymousFunction(d,f,e),a[b]=c.replace("{anonymous}",d))}}return a},guessAnonymousFunction:function(a,b){var d;
+	try{d=this.findFunctionName(this.getSource(a),b)}catch(c){d="getSource failed with url: "+a+", exception: "+c.toString()}return d},findFunctionName:function(a,b){for(var d=/function\s+([^(]*?)\s*\(([^)]*)\)/,c=/['"]?([0-9A-Za-z_]+)['"]?\s*[:=]\s*function\b/,f=/['"]?([0-9A-Za-z_]+)['"]?\s*[:=]\s*(?:eval|new Function)\b/,e="",g,j=Math.min(b,20),h,i=0;i<j;++i)if(g=a[b-i-1],h=g.indexOf("//"),0<=h&&(g=g.substr(0,h)),g){e=g+e;if((g=c.exec(e))&&g[1])return g[1];if((g=d.exec(e))&&g[1])return g[1];if((g=f.exec(e))&&
+	g[1])return g[1]}return"(?)"}};
+
+	return Triage;
+}();
+
+	/*!{id:msgpack.js,ver:1.05,license:"MIT",author:"uupaa.js@gmail.com"}*/
+
+	// === msgpack ===
+	// MessagePack -> http://msgpack.sourceforge.net/
+	this.msgpack||function(j){function o(e,b){p=0;var c=k([],e,0);return p?!1:b?w(c):c}function q(e){x="string"===typeof e?y(e):e;c=-1;return r()}function k(e,b,c){var a,d,g;if(null==b)e.push(192);else if(!1===b)e.push(194);else if(!0===b)e.push(195);else switch(typeof b){case "number":b!==b?e.push(203,255,255,255,255,255,255,255,255):Infinity===b?e.push(203,127,240,0,0,0,0,0,0):Math.floor(b)===b?0>b?-32<=b?e.push(224+b+32):-128<b?e.push(208,b+256):-32768<b?(b+=65536,e.push(209,b>>8,b&255)):-2147483648< b?(b+=4294967296,e.push(210,b>>>24,b>>16&255,b>>8&255,b&255)):(d=Math.floor(b/4294967296),b&=4294967295,e.push(211,d>>24&255,d>>16&255,d>>8&255,d&255,b>>24&255,b>>16&255,b>>8&255,b&255)):128>b?e.push(b):256>b?e.push(204,b):65536>b?e.push(205,b>>8,b&255):4294967296>b?e.push(206,b>>>24,b>>16&255,b>>8&255,b&255):(d=Math.floor(b/4294967296),b&=4294967295,e.push(207,d>>24&255,d>>16&255,d>>8&255,d&255,b>>24&255,b>>16&255,b>>8&255,b&255)):((d=0>b)&&(b*=-1),g=Math.log(b)/0.6931471805599453+1023|0,c=b*Math.pow(2, 1075-g),b=c&4294967295,d&&(g|=2048),d=c/4294967296&1048575|g<<20,e.push(203,d>>24&255,d>>16&255,d>>8&255,d&255,b>>24&255,b>>16&255,b>>8&255,b&255));break;case "string":c=b.length;g=e.length;e.push(0);for(d=0;d<c;++d)a=b.charCodeAt(d),128>a?e.push(a&127):2048>a?e.push(a>>>6&31|192,a&63|128):65536>a&&e.push(a>>>12&15|224,a>>>6&63|128,a&63|128);a=e.length-g-1;32>a?e[g]=160+a:65536>a?e.splice(g,1,218,a>>8,a&255):4294967296>a&&e.splice(g,1,219,a>>>24,a>>16&255,a>>8&255,a&255);break;default:if(++c>=D)return p= 1,[];if(E(b)){a=b.length;16>a?e.push(144+a):65536>a?e.push(220,a>>8,a&255):4294967296>a&&e.push(221,a>>>24,a>>16&255,a>>8&255,a&255);for(d=0;d<a;++d)k(e,b[d],c)}else{g=e.length;e.push(0);a=0;for(d in b)++a,k(e,d,c),k(e,b[d],c);16>a?e[g]=128+a:65536>a?e.splice(g,1,222,a>>8,a&255):4294967296>a&&e.splice(g,1,223,a>>>24,a>>16&255,a>>8&255,a&255)}}return e}function r(){var e,b,i,a=0,d,g,f=x;d=f[++c];if(224<=d)return d-256;if(192>d){if(128>d)return d;144>d?(a=d-128,d=128):160>d?(a=d-144,d=144):(a=d-160, d=160)}switch(d){case 192:return null;case 194:return!1;case 195:return!0;case 202:return a=16777216*f[++c]+(f[++c]<<16)+(f[++c]<<8)+f[++c],d=a>>23&255,g=a&8388607,!a||2147483648===a?0:255===d?g?NaN:Infinity:(a&2147483648?-1:1)*(g|8388608)*Math.pow(2,d-127-23);case 203:a=16777216*f[++c]+(f[++c]<<16)+(f[++c]<<8)+f[++c];b=a&2147483648;d=a>>20&2047;g=a&1048575;if(!a||2147483648===a)return c+=4,0;if(2047===d)return c+=4,g?NaN:Infinity;a=16777216*f[++c]+(f[++c]<<16)+(f[++c]<<8)+f[++c];return(b?-1:1)*((g| 1048576)*Math.pow(2,d-1023-20)+a*Math.pow(2,d-1023-52));case 207:return a=16777216*f[++c]+(f[++c]<<16)+(f[++c]<<8)+f[++c],4294967296*a+16777216*f[++c]+(f[++c]<<16)+(f[++c]<<8)+f[++c];case 206:a+=16777216*f[++c]+(f[++c]<<16);case 205:a+=f[++c]<<8;case 204:return a+f[++c];case 211:return a=f[++c],a&128?-1*(72057594037927936*(a^255)+281474976710656*(f[++c]^255)+1099511627776*(f[++c]^255)+4294967296*(f[++c]^255)+16777216*(f[++c]^255)+65536*(f[++c]^255)+256*(f[++c]^255)+(f[++c]^255)+1):72057594037927936* a+281474976710656*f[++c]+1099511627776*f[++c]+4294967296*f[++c]+16777216*f[++c]+65536*f[++c]+256*f[++c]+f[++c];case 210:return a=16777216*f[++c]+(f[++c]<<16)+(f[++c]<<8)+f[++c],2147483648>a?a:a-4294967296;case 209:return a=(f[++c]<<8)+f[++c],32768>a?a:a-65536;case 208:return a=f[++c],128>a?a:a-256;case 219:a+=16777216*f[++c]+(f[++c]<<16);case 218:a+=(f[++c]<<8)+f[++c];case 160:for(d=[],b=c,e=b+a;b<e;)i=f[++b],d.push(128>i?i:224>i?(i&31)<<6|f[++b]&63:(i&15)<<12|(f[++b]&63)<<6|f[++b]&63);c=b;return 10240> d.length?l.apply(null,d):w(d);case 223:a+=16777216*f[++c]+(f[++c]<<16);case 222:a+=(f[++c]<<8)+f[++c];case 128:for(g={};a--;){e=f[++c]-160;for(d=[],b=c,e=b+e;b<e;)i=f[++b],d.push(128>i?i:224>i?(i&31)<<6|f[++b]&63:(i&15)<<12|(f[++b]&63)<<6|f[++b]&63);c=b;g[l.apply(null,d)]=r()}return g;case 221:a+=16777216*f[++c]+(f[++c]<<16);case 220:a+=(f[++c]<<8)+f[++c];case 144:for(d=[];a--;)d.push(r());return d}}function w(e){try{return l.apply(this,e)}catch(b){}for(var c=[],a=0,d=e.length,g=s;a<d;++a)c[a]=g[e[a]]; return c.join("")}function t(e,b,c){function a(){if(4===h.readyState){var a,e=h.status,d={status:e,ok:200<=e&&300>e};if(!l++){if("PUT"===u)a=d.ok?h.responseText:"";else if(d.ok){if(b.worker&&j.Worker){a=new Worker(msgpack.worker);a.onmessage=function(a){c(a.data,b,d)};a.postMessage({method:"unpack",data:h.responseText});g();return}a=z?F(h):y(h.responseText);a=q(a)}n&&n(h,b,d);c(a,b,d);g()}}}function d(a,e){if(!l++){var d={status:e||400,ok:!1};n&&n(h,b,d);c(null,b,d);g(a)}}function g(a){a&&h&&h.abort&& h.abort();f&&(clearTimeout(f),f=0);h=null;j.addEventListener&&j.removeEventListener("beforeunload",d,!1)}var f=0,u=b.method||"GET",A=b.header||{},B=b.before,n=b.after,m=b.data||null,h=j.XMLHttpRequest?new XMLHttpRequest:j.ActiveXObject?new ActiveXObject("Microsoft.XMLHTTP"):null,l=0,k,o="GET"===u&&b.binary;try{h.onreadystatechange=a;h.open(u,e,!0);B&&B(h,b);o&&h.overrideMimeType&&h.overrideMimeType("text/plain; charset=x-user-defined");m&&h.setRequestHeader("Content-Type","application/x-www-form-urlencoded"); for(k in A)h.setRequestHeader(k,A[k]);j.addEventListener&&j.addEventListener("beforeunload",d,!1);h.send(m);f=setTimeout(function(){d(1,408)},1E3*(b.timeout||10))}catch(p){d(0,400)}}function y(e){var b=[],c=v,a=e.split(""),d=-1,g;g=a.length;for(e=g%8;e--;)++d,b[d]=c[a[d]];for(e=g>>3;e--;)b.push(c[a[++d]],c[a[++d]],c[a[++d]],c[a[++d]],c[a[++d]],c[a[++d]],c[a[++d]],c[a[++d]]);return b}function F(e){var b=[],c,a,d,g,f,j,k,l,n,m=-1,h;h=vblen(e);e=vbstr(e);a=Math.ceil(h/2);for(c=a%8;c--;)d=e.charCodeAt(++m), b.push(d&255,d>>8);for(c=a>>3;c--;)d=e.charCodeAt(++m),a=e.charCodeAt(++m),g=e.charCodeAt(++m),f=e.charCodeAt(++m),j=e.charCodeAt(++m),k=e.charCodeAt(++m),l=e.charCodeAt(++m),n=e.charCodeAt(++m),b.push(d&255,d>>8,a&255,a>>8,g&255,g>>8,f&255,f>>8,j&255,j>>8,k&255,k>>8,l&255,l>>8,n&255,n>>8);h%2&&b.pop();return b}function C(e){var b=[],c=0,a=-1,d=e.length,g=[0,2,1][e.length%3],c=s,f=G;if(j.btoa){for(;a<d;)b.push(c[e[++a]]);return btoa(b.join(""))}for(--d;a<d;)c=e[++a]<<16|e[++a]<<8|e[++a],b.push(f[c>> 18&63],f[c>>12&63],f[c>>6&63],f[c&63]);1<g&&(b[b.length-2]="=");0<g&&(b[b.length-1]="=");return b.join("")}j.msgpack={pack:o,unpack:q,worker:"msgpack.js",upload:function(c,b,i){b.method="PUT";b.binary=!0;if(b.worker&&j.Worker){var a=new Worker(msgpack.worker);a.onmessage=function(a){b.data=a.data;t(c,b,i)};a.postMessage({method:"pack",data:b.data})}else b.data=C(o(b.data)),t(c,b,i)},download:function(c,b,i){b.method="GET";b.binary=!0;t(c,b,i)}};var z=/MSIE/.test(navigator.userAgent),v={},s={},G="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split(""), x=[],c=0,p=0,E=Array.isArray||function(c){return"[object Array]"===Object.prototype.toString.call(c)},l=String.fromCharCode,D=512;self.importScripts&&(onmessage=function(c){"pack"===c.data.method?postMessage(C(o(c.data.data))):postMessage(q(c.data.data))});(function(){for(var c=0,b;256>c;++c)b=l(c),v[b]=c,s[c]=b;for(c=128;256>c;++c)v[l(63232+c)]=c})();z&&document.write('<script type="text/vbscript">Function vblen(b)vblen=LenB(b.responseBody)End Function\nFunction vbstr(b)vbstr=CStr(b.responseBody)+chr(0)End Function<\/script>')}(this);
+
